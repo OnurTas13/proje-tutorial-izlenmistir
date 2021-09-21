e2d54fcdab71f61a52696a93e1a585689fccf9fb
OnurTas13 <port_o@hotmail.com>
2021-09-21 19:06:48 +0300

    .

---
+++ Frontend/src/app/pipes/filter.pipe.ts
@@ -0,0 +1,41 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+ //@ts-ignore
+  transform(value: any[], filterString: string, propName: string): any[] {
+
+    const resultArray = [];
+    if (value) {
+
+      if (value.length === 0 || filterString === '' || propName === '') {
+        return value;
+      }
+
+      for (const item of value) { 
+        if (typeof item[propName] == 'string') {
+          filterString.toLocaleLowerCase();
+          if (filterString) {
+            return value.filter((i) => i[propName].toLocaleLowerCase().indexOf(filterString) !== -1);
+          } else {
+            return value;
+          }
+          break;
+        }
+        else {
+          for (const item2 of value) {
+            if (item2[propName] === filterString) {
+              resultArray.push(item);
+            }
+          }
+          return resultArray;
+        }
+        
+      }
+
+    }
+  }
+}

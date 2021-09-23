92a825497561398bc5b0cf4ea3c4df0ad43dfb52
OnurTas13 <port_o@hotmail.com>
2021-09-23 15:46:45 +0300

    .

---
+++ Backend/projeBackend/projeBackend/Controllers/CityController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Atlanta", "New York", "Chicago" };
+        }
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Atlanta";
+        }
+    }
+}
